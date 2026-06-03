adf75912b4d046f0924c37cfac5c344c81be3334
gordod3 <dr.gordod@mail.ru>
2026-06-03 12:16:11 +0600

    Add Goblin Craftable Items

--- Content.Server/_NF/Species/Components/GoblinComponent.cs
+++ Content.Server/_NF/Species/Components/GoblinComponent.cs
@@ -5,3 +5,19 @@ public sealed partial class GoblinComponent : Component
 {
 
 }
+
+[RegisterComponent]
+public sealed partial class GoblinMadeComponent : Component { }
+
+[RegisterComponent]
+public sealed partial class GoblinClothingRobesComponent : Component { }
+
+[RegisterComponent]
+public sealed partial class GoblinVehicleComponent : Component { }
+
+[RegisterComponent]
+public sealed partial class GoblinPreciousTrashComponent : Component { }
+
+[RegisterComponent]
+public sealed partial class GoblinPreciousTrashBagComponent : Component { }
+

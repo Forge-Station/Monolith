b1e0d913bfd57b678cee97e0f100246f7502bfb5
artur <artur.idrisov.01@mail.ru>
2026-07-04 14:47:09 +0300

    переименование ивента.

--- Content.Shared/_Forge/ShowRoleInformation/ShowRoleInformationFromServerEvent.cs
+++ Content.Shared/_Forge/ShowRoleInformation/ShowRoleInformationFromServerEvent.cs
@@ -3,7 +3,7 @@
 namespace Content.Shared._Forge.ShowRoleInformation;
 
 [Serializable, NetSerializable]
-public sealed class ShowRoleInformationEvent : EntityEventArgs
+public sealed class ShowRoleInformationFromServerEvent : EntityEventArgs
 {
     public string Description { get; set; } = string.Empty;
     public string RoleName { get; set; } = string.Empty;

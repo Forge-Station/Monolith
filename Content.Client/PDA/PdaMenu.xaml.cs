eaf4e4ea9b8d3508ef48c893c6b23f3548d138b0
teasum <playm56b@gmail.com>
2026-08-13 20:30:33 +0300

    выпилил всю войну из кпк(теперь точно)

--- Content.Client/PDA/PdaMenu.xaml.cs
+++ Content.Client/PDA/PdaMenu.xaml.cs
@@ -241,8 +241,8 @@ namespace Content.Client.PDA
             /* #Forge-Change нам это пока не надо
             var warLevel = state.PdaOwnerInfo.WarLevel;
             WarLevelLabel.SetMarkup(warLevel != null ? warLevel : Loc.GetString("comp-pda-ui-station-war-level-unknown"));
-            */
             AddressLabel.Text = state.Address?.ToUpper() ?? " - ";
+            */
 
             EjectIdButton.IsActive = state.PdaOwnerInfo.IdOwner != null || state.PdaOwnerInfo.JobTitle != null;
             EjectPenButton.IsActive = state.HasPen;

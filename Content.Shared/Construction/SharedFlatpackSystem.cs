cb9dbed0cfde5a9ce988f1d21cbbfbd20b5f2b19
teasum <playm56b@gmail.com>
2026-08-22 23:08:19 +0300

    фикс коментариев

--- Content.Shared/Construction/SharedFlatpackSystem.cs
+++ Content.Shared/Construction/SharedFlatpackSystem.cs
@@ -114,11 +114,13 @@ public abstract partial class SharedFlatpackSystem : EntitySystem
     {
         if (!args.IsInDetailsRange)
             return;
-        if (PrototypeManager.TryIndex(ent.Comp.QualityNeeded, out var qualityProto)) //Forge-Change-Start
+         //Forge-Change-Start
+        if (PrototypeManager.TryIndex(ent.Comp.QualityNeeded, out var qualityProto))
         {
             var toolName = Loc.GetString(qualityProto.Name);
             args.PushMarkup(Loc.GetString("flatpack-examine", ("tool", toolName)));
-        }//Forge-Change-End
+        }
+        //Forge-Change-End
     }
 
     protected void SetupFlatpack(Entity<FlatpackComponent?> ent, EntProtoId proto, EntityUid board)

20ff8717630c96adc219ce33863153d0d6d353cf
artur <artur.idrisov.01@mail.ru>
2026-09-04 15:14:33 +0300

    Сообщение для смены режимов окна map в консоли пилотирования шаттлом

---
+++ Content.Shared/_Forge/Shuttles/BUIStates/MapToggleModShuttleMessage.cs
@@ -0,0 +1,6 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared.Shuttles.BUIStates;
+
+[Serializable, NetSerializable]
+public sealed class MapToggleModShuttleMessage : BoundUserInterfaceMessage;
